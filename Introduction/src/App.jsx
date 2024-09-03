baf4751a7eeb0b3017e984bbac8d6b61245bf79d
SyedFazal110 <syedfazal808@gmail.com>
2024-09-03 17:47:00 +0500

    react Introduction

---
+++ Introduction/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Card from './components/Cards'
+import Button from './components/button'
+import "./index.css"
+
+
+function App() {
+ 
+  return (
+    <>
+    <h1>Hello World!</h1>
+     {/* <Card title="cars" desc="good cars" price="100000"/>
+     <button onClick={Button}>Click</button> */}
+    </>
+  )
+}
+
+export default App
---
+++ Introduction/src/components/Cards.jsx
@@ -0,0 +1,15 @@
+
+
+const Card = ({title , desc , price})=>{
+
+   return(
+    <>
+    <h1>Cards</h1>
+    <h2>{title}</h2>
+    <p>{desc}</p>
+    <p>{price}</p>
+    </>
+   )
+}
+
+export default Card
